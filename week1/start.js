4919727a508044917dd4cb8f37e0e3fe29794b62
Michael Vanderpoorten <michael@yungpanda.com>
2022-09-19 13:30:34 +0200

    ✨ Les 1

--- week1/start.js
+++ week1/start.js
@@ -21,10 +21,48 @@ var b = 16;
 
 // new school = let & const
 let c = 10;
+let e;
 console.log(c);
 c = 20;
 console.log(c);
 
 const d = 10;
-// d = 20; -> error
+// d = 20; // -> error
 console.log(d);
+
+const name = "Michael Vanderpoorten"; // string
+const isLector = true; // boolean (true or false)
+const porto = 0; // number
+const club = 4; // number
+const pi = 3.14; // number (float)
+const car = null; // null
+
+// strings advanced
+console.log(firstName + " " + lastName);
+
+// optie 1
+// const sentence = "De beer zei \"hallo\"";
+// optie 2
+const sentence = 'De beer zei "hallo"';
+
+const work = "lector";
+// doel: ik ben Michael en mijn beroep is lector
+// optie 1
+console.log("Ik ben " + firstName + " en mijn beroep is " + work);
+// optie 2
+console.log(`Ik ben ${firstName} en mijn beroep is ${work}`);
+
+// multi lines
+console.log("Hallo, \nwelkom in mijn les");
+console.log(`Hallo,
+welkom in mijn les`);
+
+// numbers
+const width = 100 * 50;
+console.log(width);
+const sum = a + b + c;
+console.log(sum);
+const length = 100 / 50;
+console.log(length);
+const min = 100 - a;
+console.log(min);
