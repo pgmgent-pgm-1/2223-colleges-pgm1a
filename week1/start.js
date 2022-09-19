8cd796a6e153a4acf514ca6b8182576fbf8c99c1
Michael Vanderpoorten <michael@yungpanda.com>
2022-09-19 11:11:12 +0200

    ✨ Les 1

---
+++ week1/start.js
@@ -0,0 +1,30 @@
+/*
+Programming 1: Essentials
+College Week 1
+*/
+console.log("Hello world");
+console.log("Hallo 1PGMA");
+
+// Console log name with variable
+const firstName = "Michael";
+const lastName = "Vanderpoorten";
+console.log(firstName);
+console.log(lastName);
+
+// variables with var (= old-school)
+var a; // waarde = undefined
+console.log(a);
+a = 31; // waarde = 31;
+a = 20;
+console.log(a);
+var b = 16;
+
+// new school = let & const
+let c = 10;
+console.log(c);
+c = 20;
+console.log(c);
+
+const d = 10;
+// d = 20; -> error
+console.log(d);
