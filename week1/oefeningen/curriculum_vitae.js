d1d9b3d7e484bef7378cb59cc07f4ff5a3f94778
Michael Vanderpoorten <michael@yungpanda.com>
2022-09-19 15:40:31 +0200

    ✨ Oefening week 1

---
+++ week1/oefeningen/curriculum_vitae.js
@@ -0,0 +1,34 @@
+const lastName = "Vanderpoorten";
+const firstName = "Michael";
+const gender = "male";
+const age = 30;
+const married = false;
+const avatar = "\u{1F344}";
+const phrase = "Euhm";
+
+const languageDutch = "**********";
+const languageEnglish = "**********";
+const languageFrench = "******";
+const languageGerman = "***";
+const languageSpanish = "***";
+
+console.log(`
+My personal Curriculum Vitae
+============================================================
+First name:  ${firstName}
+Last name: ${lastName}
+Gender: ${gender}
+Age: ${age}
+Married: ${married}
+Avatar: ${avatar}
+Phrase: ${phrase}
+------------------------------------------------------------
+Languages
+------------------------------------------------------------
+Dutch:      ${languageDutch}
+English:    ${languageEnglish}
+French      ${languageFrench}
+German      ${languageGerman}
+Spanish     ${languageSpanish}
+============================================================
+`);
